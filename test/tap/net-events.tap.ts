fcb3cb0848ccbe74d87a9deacdaf6e40789edeba
Matt Kuritz <mkuritz@thefarmersdog.com>
2025-05-07 17:53:18 -0400

    more missing binds

--- test/tap/net-events.tap.ts
+++ test/tap/net-events.tap.ts
@@ -19,6 +19,7 @@ test('continuation-local state with net connection', function(t) {
 
       server = net.createServer(function(socket) {
         t.equal(namespace.get('test'), 'newContextValue', 'state has been mutated');
+        namespace.bindEmitter(socket);
         socket.on('data', function() {
           t.equal(namespace.get('test'), 'newContextValue', 'state is still preserved');
           server.close();
@@ -32,6 +33,7 @@ test('continuation-local state with net connection', function(t) {
           const client = net.connect(address.port, 'localhost', function() {
             t.equal(namespace.get('test'), 'MONKEY', 'state preserved for client connection');
             client.write('Hello');
+            namespace.bindEmitter(client);
             client.on('data', function() {
               t.equal(namespace.get('test'), 'MONKEY', 'state preserved for client data');
               t.end();
